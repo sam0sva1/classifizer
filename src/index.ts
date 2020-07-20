bb0651fc4fe3155151c9cf9bf4509daf13d74f03
Novosad Konstantin Valentinovich <konstantus@live.ru>
2020-07-20 15:35:24 +0300

    CFZ reworks exports 2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export * from './lib/index';
+import { classes } from './lib/classes';
+import { styledClasses } from './lib/styledClasses';
+import { stylize } from './lib/stylize';
+import { classesStylizer } from './lib/classesStylizer';
+import { stringStylizer } from './lib/stringStylizer';
+
+export {
+  classes,
+  styledClasses,
+  stylize,
+  classesStylizer,
+  stringStylizer,
+}
+
+export default {
+  classes,
+  styledClasses,
+  stylize,
+  classesStylizer,
+  stringStylizer,
+}
